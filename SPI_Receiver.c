a41ca24472dbed54bc551398a40bd0905a954565
navi931 <47607235+navi931@users.noreply.github.com>
2019-03-06 22:02:22 -0600

    Add files via upload

--- SPI_Receiver.c
+++ SPI_Receiver.c
@@ -2,7 +2,6 @@
 #include"GPIOHandler.h"
 #include"GPIO.h"
 
-
 #define ENABLE 			0b01010000
 #define DISABLE 		0b01000000
 #define TURN_ON 		0b00010000
@@ -16,9 +15,10 @@
 #define YELLOW			0b00001000
 #define PLACA 			0b00010010
 
+#define TOP 			48
 typedef unsigned char uint_8;
 
-void ExecComands(void);
+
 void Enable (uint_8 led);
 void Disable (uint_8 led);
 void TurnOn (uint_8 led);
@@ -27,21 +27,19 @@ void Toogle (uint_8 led);
 void Erase (uint_8 led);
 void SendColor(uint_8 led);
 void ImprimirContador (uint_8 contador);
+uint_8 valueinarray(uint_8 valor, const uint_8 *arr,uint_8 size);
+uint_8 checkArrays(uint_8 * array1,uint_8 * array2,uint_8 size);
+void readingState(void);
+void idleState(void);
+void ExecComands(void);
+void copyArrays(uint_8 * destiny,uint_8 * source,uint_8 size);
+void cleanArray(uint_8 * source,uint_8 size);
 
-enum function{
-eEnable,
-eDisable,
-eTurnOn,
-eTurnOff,
-eToggle,
-eErase};
 
-enum colores{
-eGreen,
-eWhite,
-eRed,
-eYellow,};
-
+/*
+ * En la siguientes lineas declararemos las maquinas de estados que utilizaremos
+ * con sus respectivos enum
+ */
 void (*StateMachine[])(uint_8 led) =
 {
 	Enable,							//0
@@ -51,8 +49,62 @@ void (*StateMachine[])(uint_8 led) =
 	Toogle,							//4
 	Erase							//5
 };
+enum function{
+eEnable,
+eDisable,
+eTurnOn,
+eTurnOff,
+eToggle,
+eErase};
 
+
+void (*reading_states[])(void) =
+{
+	readingState,
+	idleState
+};
+enum Decoder
+{
+	sReading,
+	sIdle
+};
+
+/*
+ * Ahora haremos que tenemos las maquina de estados preparadas inicializaremos
+ * las variables que ocuparemos en lo largo del programa
+ */
+/*
+ * Variables compartidas entre las dos maquinas de estados
+ */
+
+uint_8 ready = 0;
+
+/*
+ * Lo siguiente son las variables que utilizaremos para estar leyendo en del SPI
+ */
+const uint_8 validColor[]={GREEN,WHITE,RED,YELLOW};
+const uint_8 validCommand[]={ENABLE,DISABLE,TURN_ON,TURN_OFF,TOGGLE,ERASE};
+
+uint_8 temporal[50]={0};
+uint_8 spi = 0;
+
+uint_8 state=sIdle;
+
+uint_8 * ptrtemporal;
+uint_8 * ptrspi = &spi;
+enum Trama
+{
+	eStart,
+	eIteraciones,
+	eElem1
+};
+
+/*
+ * Ahora declararemos las variables que utilizaremos para
+ * ejecutar los comandos
+ */
 uint_8 data [50] = {0xFE,5,ENABLE|GREEN,TURN_ON|GREEN,DISABLE|GREEN,TURN_ON|GREEN,TURN_ON|GREEN};
+
 
 uint_8 enable[7] =   {'e','n','a','b','l','e',' '};
 uint_8 disable[8] =  {'d','i','s','a','b','l','e',' '};
@@ -65,24 +117,24 @@ uint_8 green[6] =    {'g','r','e','e','n',' '};
 uint_8 white[6] =    {'w','h','i','t','e',' '};
 uint_8 red[4] =   	 {'r','e','d',' '};
 uint_8 yellow[7] =   {'y','e','l','l','o','w',' '};
-
-
 uint_8 estados_colores[4] = {0,0,0,0};
 
+enum colores{
+eGreen,
+eWhite,
+eRed,
+eYellow,};
 
+/*
+ * Ya inicializado todo esto podemos trabajar
+ */
 int main (void)
 {
 	COMM_vfnDriverInitSlave();
-	volatile uint_8 i = 0;
-	EnableLed(GREEN);
-	TurnOnLed(GREEN);
 	while(1)
 	{
-
-		ToggleLed(GREEN);
+		reading_states[state]();
 		ExecComands();
-		i++;
-		i--;
 	}
 }
 
@@ -96,7 +148,7 @@ void ExecComands(void)
 
 	if(data[0] == 0xFE)
 	{
-		if(contador < data[1])
+		if((contador < data[1]) && (data[1] != 0))
 		{
 			instruccion = (*ptrdata & 0b11110000);
 			led = (*ptrdata & 0b00001111);
@@ -134,14 +186,31 @@ void ExecComands(void)
 			ptrdata++;
 			contador++;
 		}
-		else
+		else if(contador >= data[1])
 		{
-			ptrdata = &data[2];
 			contador = 0;
+			ptrdata = &data[2];
+			reiniciar();
 		}
+	}
+	else
+	{
+		contador = 0;
+		ptrdata = &data[2];
+		reiniciar();
 	}
 }
 
+void reiniciar (void)
+{
+	StateMachine[eErase](led);
+	if(ready)
+	{
+		ready = 0;
+		copyArrays(&data[0],&temporal[0],sizeof(temporal));
+		cleanArray(&temporal[0],sizeof(temporal));
+	}
+}
 void ImprimirContador (uint_8 contador)
 {
 	uint_8 numero_temp = 0;
@@ -323,3 +392,110 @@ void SendColor(uint_8 led)
 		COMM_bfnSendMsg(&yellow[0],sizeof (yellow)/sizeof (uint_8));
 	}
 }
+
+uint_8 valueinarray(uint_8 valor, const uint_8 *arr,uint_8 size)
+{
+		uint_8 i=0;
+		for(i=0;i<size;i++)
+		{
+			if(*(arr+i)==valor)
+			{
+				return SUCCESS;
+			}
+		}
+	return ERROR;
+}
+
+uint_8 checkArrays(uint_8 * array1,uint_8 * array2,uint_8 size)
+{
+	uint_8 i = 0;
+	uint_8 state = SUCCESS;
+	for(;i<size;i++,array1++,array2++)
+	{
+		if(*array1 != *array2)
+		{
+			state = ERROR;
+		}
+	}
+	return state;
+}
+
+
+void idleState(void)
+{
+	COMM_bfnReceiveMosi(ptrspi);
+	if(spi==0xFE)
+	{
+		ptrtemporal = &temporal[eStart];
+		*ptrtemporal = spi;
+		state = sReading;
+		ptrtemporal = &temporal[eIteraciones];
+	}
+}
+
+
+void readingState(void)
+{
+
+	static uint_8 tmpCount=0;
+	static uint_8 contador=0;
+	uint_8 valCol=ERROR;
+	uint_8 valCom=ERROR;
+
+	uint_8 instruccion;
+	uint_8 led;
+
+	if(ptrtemporal == &temporal[eIteraciones])
+	{
+		COMM_bfnReceiveMosi(ptrspi);
+		*ptrtemporal = spi;
+		tmpCount = 2;
+	}
+
+	if((temporal[eIteraciones]<=TOP) && (temporal[eIteraciones]!=0))
+	{
+		COMM_bfnReceiveMosi(ptrspi);
+		ptrtemporal = &temporal[eElem1+contador];
+		*ptrtemporal = spi;
+	}
+
+	if((contador<temporal[eIteraciones]) && (temporal[eIteraciones]<=48) && (temporal[eIteraciones]!=0))
+	{
+		instruccion=(*(ptrtemporal + contador))& 0b11110000;
+		led=(*(ptrtemporal + contador))&0b00001111;
+
+		valCol = valueinarray(led,&validColor[0],sizeof(validColor));
+		valCom = valueinarray(instruccion,&validCommand[0],sizeof(validCommand));
+
+		if(valCol & valCom)
+		{
+			tmpCount++;
+			contador++;
+		}
+	}
+
+	if((contador >= temporal[eIteraciones]) && (temporal[eIteraciones]!=0))
+	{
+		state=sIdle;
+		ready = 1;
+	}
+}
+
+void copyArrays(uint_8 * destiny,uint_8 * source,uint_8 size)
+{
+	uint_8 i = 0;
+	for(;i<size;i++,destiny++,source++)
+	{
+		*destiny = *source;
+	}
+}
+void cleanArray(uint_8 * source,uint_8 size)
+{
+	uint_8 i = 0;
+	for(;i<size;i++,source++)
+	{
+		*source = 0;
+	}
+}
+
+
